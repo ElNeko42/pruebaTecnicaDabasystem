2af887319170c5ac2658db7a8108ea97e6eef688
FranVilatec <fben@vilatec.com>
2026-09-09 12:20:20 +0200

    Refactor Vuetify initialization to include components and directives

--- resources/js/main.js
+++ resources/js/main.js
@@ -3,6 +3,8 @@ import '@mdi/font/css/materialdesignicons.css';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 import axios from 'axios';
 import App from './App.vue';
 
@@ -10,5 +12,5 @@ axios.defaults.baseURL = '/api';
 axios.defaults.headers.common.Accept = 'application/json';
 const token = localStorage.getItem('admin_token');
 if (token) axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-const vuetify = createVuetify({ theme: { defaultTheme: 'light' } });
+const vuetify = createVuetify({ components, directives, theme: { defaultTheme: 'light' } });
 createApp(App).use(createPinia()).use(vuetify).mount('#app');
